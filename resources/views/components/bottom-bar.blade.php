aab667a8551e406dd7b9119fce8056b84bfb4973
luqni <luqnibaehaqi@detik.com>
2024-11-10 14:35:10 +0700

    refactor: active unactive menu on bootom menu

--- resources/views/components/bottom-bar.blade.php
+++ resources/views/components/bottom-bar.blade.php
@@ -2,31 +2,31 @@
     <div class="container p-0">
         <ul class="navbar-nav nav-flex-icons">
             <li class="nav-item flex-fill">
-                <a class="nav-link p-0 active" href="{{ route('home') }}">
+                <a class="nav-link p-0 {{ request()->routeIs('home') ? 'active' : '' }}" href="{{ route('home') }}">
                     <span class="icon"><i class="material-icons">home</i></span>
                     <span class="nav-title">Home</span>
                 </a>
             </li>
             <li class="nav-item flex-fill">
-                <a class="nav-link p-0" href="{{ route('campaigns.index') }}">
+                <a class="nav-link p-0 {{ request()->routeIs('campaigns.index') ? 'active' : '' }}" href="{{ route('campaigns.index') }}">
                     <span class="icon"><i class="material-icons">article</i></span>
                     <span class="nav-title">Kanal</span>
                 </a>
             </li>
             <li class="nav-item flex-fill">
-                <a class="nav-link p-0" href="{{ route('register.organization') }}">
+                <a class="nav-link p-0 {{ request()->routeIs('register.organization') ? 'active' : '' }}" href="{{ route('register.organization') }}">
                     <span class="icon"><i class="material-icons">campaign</i></span>
                     <span class="nav-title">Galang Dana</span>
                 </a>
             </li>
             <li class="nav-item flex-fill">
-                <a class="nav-link p-0" href="{{ route('my-transactions.index') }}">
+                <a class="nav-link p-0 {{ request()->routeIs('my-transactions.index') ? 'active' : '' }}" href="{{ route('my-transactions.index') }}">
                     <span class="icon"><i class="material-icons">list_alt</i></span>
                     <span class="nav-title">Transaksi</span>
                 </a>
             </li>
             <li class="nav-item flex-fill">
-                <a class="nav-link p-0" href="{{ route('profile.edit') }}">
+                <a class="nav-link p-0 {{ request()->routeIs('profile.edit') ? 'active' : '' }}" href="{{ route('profile.edit') }}">
                     <span class="icon"><i class="material-icons">account_circle</i></span>
                     <span class="nav-title">Profil</span>
                 </a>
